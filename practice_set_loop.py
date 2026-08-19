011505840eb1aff961897c3a2cfc8450417c1a29
apsanamajhi303-pixel <254070477+apsanamajhi303-pixel@users.noreply.github.com>
2026-08-19 19:43:06 +0545

    Delete practice_set_loop.py

--- practice_set_loop.py
+++
@@ -1,8 +0,0 @@
-#Print the multiplication table of a given number using a for loop.
-num=int(input("Enter a number: "))
-for i in range(1,11):
-    print(f"{num} * {i}={num * i}")
-
-#Greet everyone in a list whose name starts with S.
-name =["Seb","Saru","Sandhya","Shila","Sima"]
-for name in range(1,5):
